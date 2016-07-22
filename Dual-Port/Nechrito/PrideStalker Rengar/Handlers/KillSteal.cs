226cbf88fd601a5b9afbb3191341a4ad3d9ab3b3
berbb <tintinridge@gmail.com>
2016-07-21 21:41:26 -0500

    PrideStalker Rengar
    - Ported updates

--- Dual-Port/Nechrito/PrideStalker Rengar/Handlers/KillSteal.cs
+++ Dual-Port/Nechrito/PrideStalker Rengar/Handlers/KillSteal.cs
@@ -1,42 +1,32 @@
-﻿using EloBuddy;
-using LeagueSharp.SDK;
+﻿using LeagueSharp.SDK;
 using PrideStalker_Rengar.Main;
-using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using LeagueSharp.SDK.Enumerations;
 
- namespace PrideStalker_Rengar.Handlers
+namespace PrideStalker_Rengar.Handlers
 {
     class KillSteal : Core
     {
-       
+
         public static void Killsteal()
         {
-            foreach (var target in GameObjects.EnemyHeroes.Where(x => x.LSIsValidTarget(800) && !x.IsDead && !x.IsZombie))
+            foreach (var target in GameObjects.EnemyHeroes.Where(x => x.LSIsValidTarget(800) && !x.IsDead && !x.IsZombie).Where(target => target.LSIsValidTarget()))
             {
-                if(target != null && target.LSIsValidTarget())
+                if (Spells.E.IsReady() && target.Health < Spells.E.GetDamage(target))
                 {
-                    if(Spells.E.IsReady() && target.Health < Spells.E.GetDamage(target))
-                    {
-                        Spells.E.Cast(target);
-                    }
-                    if (Spells.W.IsReady() && target.Health < Spells.W.GetDamage(target))
-                    {
-                        Spells.W.Cast(target);
-                    }
+                    Spells.E.CastIfHitchanceEquals(target, HitChance.High);
+                }
+                if (Spells.W.IsReady() && target.Health < Spells.W.GetDamage(target))
+                {
+                    Spells.W.Cast(target);
                 }
             }
-            if(MenuConfig.KillStealSummoner)
+
+            if (!MenuConfig.KillStealSummoner) return;
+
+            foreach (var target in GameObjects.EnemyHeroes.Where(t => t.LSIsValidTarget(600f)).Where(target => target.Health < Dmg.IgniteDmg && Spells.Ignite.IsReady()))
             {
-                foreach (var target in GameObjects.EnemyHeroes.Where(t => t.LSIsValidTarget(600f)))
-                {
-                    if (target.Health < Dmg.IgniteDmg && Spells.Ignite.IsReady())
-                    {
-                        GameObjects.Player.Spellbook.CastSpell(Spells.Ignite, target);
-                    }
-                }
+                GameObjects.Player.Spellbook.CastSpell(Spells.Ignite, target);
             }
         }
     }
